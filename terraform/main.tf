ff69fa5e2940d0eaeab085d81e5905f5e407da99
RangaTpst <nicolas.rouillelanoe@gmail.com>
2026-06-04 10:48:39 +0200

    feat: terraform gère toute l'infra (MiniStack + S3)

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
   }
 }
 
@@ -14,14 +18,33 @@ provider "aws" {
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
-  s3_use_path_style         = true
+  s3_use_path_style           = true
 
   endpoints {
     s3 = "http://localhost:4566"
-    # Ajoutez d'autres services ici si nécessaire
   }
 }
 
+provider "docker" {}
+
+# MiniStack
+resource "docker_image" "ministack" {
+  name = "ministackorg/ministack"
+}
+
+resource "docker_container" "ministack" {
+  name  = "ministack"
+  image = docker_image.ministack.image_id
+
+  ports {
+    internal = 4566
+    external = 4566
+  }
+}
+
+# Bucket S3
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "my-test-bucket-for-students"
+
+  depends_on = [docker_container.ministack]
 }
