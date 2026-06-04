0fd51bd35a5bb870cfbd2bb0287159d9dbf90b88
RangaTpst <nicolas.rouillelanoe@gmail.com>
2026-06-04 09:46:12 +0200

    init: terraform + next.js app avec HTTPS local

---
+++ terraform/main.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region                      = "us-east-1"
+  access_key                  = "test"
+  secret_key                  = "test"
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
+  s3_use_path_style         = true
+
+  endpoints {
+    s3 = "http://localhost:4566"
+    # Ajoutez d'autres services ici si nécessaire
+  }
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-test-bucket-for-students"
+}
